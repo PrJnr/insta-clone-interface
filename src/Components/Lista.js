414f92313e5f9001df58f32b7274543f9272829c
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-11-21 20:09:25 -0300

    component Lista for render items in feed

---
+++ src/Components/Lista.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class Lista extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <View>
+                <Text>Feed...</Text>
+            </View>
+        );
+    }
+}
+
+export default Lista;
