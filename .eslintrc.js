6e43af08e0fb343112379e020a0127b08a7b37d3
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-11-21 19:11:26 -0300

    config prettier and eslint, solving errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier',
-    'prettier/react',
-  ],
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    'prettier',
-  ],
-  rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': [ 'warn',
-      { extensions: ['.jsx', '.js'] } ],
-      'import/prefer-default-export': 'off',
-  },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'prettier/react',
+    ],
+    parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        'prettier',
+    ],
+    rules: {
+        'prettier/prettier': 'error',
+        'react/jsx-filename-extension': ['warn',
+            { extensions: ['.jsx', '.js'] }],
+        'import/prefer-default-export': 'off',
+    },
 };
