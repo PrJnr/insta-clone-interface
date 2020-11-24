9f24dbccc2b9d709f45e7c880e25d08d3c0482eb
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2020-11-23 22:23:53 -0300

    Header

--- App.js
+++ App.js
@@ -1,17 +1,111 @@
+/* eslint-disable global-require */
+/* eslint-disable react/no-unused-state */
+/* eslint-disable no-use-before-define */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    FlatList,
+} from 'react-native';
+
+import Lista from './src/Components/Lista';
 
 // import { Container } from './styles';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            feed: [
+                {
+                    id: '1',
+                    nome: 'prjr_dexter',
+                    imgperfil:
+                        'https://instagram.fbau3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/119878272_124929889060538_6937223157872859220_n.jpg?_nc_ht=instagram.fbau3-2.fna.fbcdn.net&_nc_ohc=7_NsU38pGVAAX8AJKua&tp=1&oh=bbddc61009eeb47e69ded2469fcd2c7a&oe=5FE4652A',
+                    imgPublicacao:
+                        'https://veja.abril.com.br/wp-content/uploads/2016/05/alx_lista-capitais-europa-mundo-20100806-002_original3.jpeg',
+                    curtida: false,
+                    likers: '',
+                },
+                {
+                    id: '2',
+                    nome: 'gabrielli.rs',
+                    imgperfil:
+                        'https://instagram.fbau3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/119878272_124929889060538_6937223157872859220_n.jpg?_nc_ht=instagram.fbau3-2.fna.fbcdn.net&_nc_ohc=7_NsU38pGVAAX8AJKua&tp=1&oh=bbddc61009eeb47e69ded2469fcd2c7a&oe=5FE4652A',
+                    imgPublicacao:
+                        'https://veja.abril.com.br/wp-content/uploads/2016/05/alx_lista-capitais-europa-mundo-20100806-002_original3.jpeg',
+                    curtida: false,
+                    likers: '',
+                },
+                {
+                    id: '3',
+                    nome: 'Mathues',
+                    imgperfil:
+                        'https://instagram.fbau3-2.fna.fbcdn.net/v/t51.2885-19/s150x150/119878272_124929889060538_6937223157872859220_n.jpg?_nc_ht=instagram.fbau3-2.fna.fbcdn.net&_nc_ohc=7_NsU38pGVAAX8AJKua&tp=1&oh=bbddc61009eeb47e69ded2469fcd2c7a&oe=5FE4652A',
+                    imgPublicacao:
+                        'https://veja.abril.com.br/wp-content/uploads/2016/05/alx_lista-capitais-europa-mundo-20100806-002_original3.jpeg',
+                    curtida: false,
+                    likers: '',
+                },
+            ],
+        };
+    }
+
     render() {
+        const { state } = this;
         return (
-            <View>
-                <Text>TESTE</Text>
+            <View style={styles.container}>
+                <View style={styles.header}>
+                    <TouchableOpacity>
+                        <Image
+                            source={require('./src/img/logo.png')}
+                            style={styles.logo}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Image
+                            source={require('./src/img/send.png')}
+                            style={styles.send}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <FlatList
+                    keyExtractor={(item) => item.id}
+                    showsVerticalScrollIndicator={false}
+                    data={state.feed}
+                    renderItem={({ item }) => <Lista data={item} />}
+                />
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        height: 55,
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        padding: 5,
+        borderBottomWidth: 0.2,
+        shadowColor: '#9991',
+        elevation: 1,
+    },
+    logo: {
+        marginLeft: 10,
+    },
+    send: {
+        width: 23,
+        height: 23,
+    },
+});
 
 export default App;
